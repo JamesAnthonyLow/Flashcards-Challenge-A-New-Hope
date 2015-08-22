4f92a4081d144c4be996c9971df4462a575683a9
ZedforZardoz <loweja01@gmail.com>
2015-08-22 10:36:47 -0400

    add tests for guest login and invalid user password

--- spec/features/user_authorization_feature_spec.rb
+++ spec/features/user_authorization_feature_spec.rb
@@ -9,12 +9,28 @@ feature "user_authorization" do
 		User.delete_all
 	end
 
-	scenario "Allows user to login" do
-			visit "/user/login/"
+	scenario "Allows user to login with valid password" do
+			visit "/login"
 			#fill_in accepts name attribute as parameter
 			fill_in "username", with: "James"
 			fill_in "password_hash", with: "1234"
 			click_button "Log in"
 			expect(page.current_path).to eq "/" #welcome page
+	end
+
+	scenario "Does not allow user to login with invalid password" do
+			visit "/login"
+			#fill_in accepts name attribute as parameter
+			fill_in "username", with: "James"
+			fill_in "password_hash", with: "5555"
+			click_button "Log in"
+			expect(page.current_path).to eq "/login" #or error page?
+			# expect(page).to have_content("write error message")
+	end
+
+	scenario "Allows user to login as guest" do
+		visit "/login"
+		click_button "Guest"
+		expect(page.current_path).to eq "/" #welcome page
 	end
 end
