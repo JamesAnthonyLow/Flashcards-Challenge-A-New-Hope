2b9cf0a1395a4c8c40ef985007a1bd98a7dc2fdd
ZedforZardoz <loweja01@gmail.com>
2015-08-21 21:09:34 -0400

    add columns to migrations

--- db/migrate/20150821204028_create_users.rb
+++ db/migrate/20150821204028_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+  	create_table :users do |t|
+  		t.string :username, null: false, limit: 64
+  		t.string :avatar_src, null: false
+  		t.string :password_hash
+
+  		t.timestamps
+  	end
   end
 end
--- db/migrate/20150821204056_create_decks.rb
+++ db/migrate/20150821204056_create_decks.rb
@@ -1,4 +1,9 @@
 class CreateDecks < ActiveRecord::Migration
   def change
+  	create_table :decks do |t|
+  		t.string :name, null: false, limit: 64
+
+  		t.timestamps
+  	end
   end
 end
