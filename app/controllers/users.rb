528a5ff3a3dc1e5d741d4ae2133c9acbc5e3f5a9
GLNRO <lg@glnro.com>
2015-08-22 21:09:20 -0400

    Add unauth logic to user show page

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,4 +1,8 @@
 get "/profile/:id" do
-  user = User.find_by(id: session[:user_id])
-  erb :"users/profile", locals: {user: user}
+	if params[:id] == current_user.id	
+  		user = User.find_by(id: session[:user_id])
+  		erb :"users/profile", locals: {user: user}
+  	else
+  		erb :"auth/unauth"
+  	end
 end
