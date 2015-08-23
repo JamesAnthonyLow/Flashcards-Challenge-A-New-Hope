ddaab43b0750d5807a236527dd06728f4c5d43e5
Ramon Ramos III <email": null>
2015-08-23 13:39:44 -0400

    Refactor score controller

--- app/controllers/score.rb
+++ app/controllers/score.rb
@@ -1,5 +1,12 @@
 get "/game/:id/score" do
 	@game = Game.find_by(id: params[:id])
   @deck = @game.deck
+
 	erb :"scores/show"
+end
+
+get "/user/:id/scores" do
+  @user = User.find_by(id: params[:id])
+
+  erb :"users/user_scores"
 end
