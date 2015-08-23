bd8f2c4dd2b9f1c3f53c1056725b910dbf41fa0b
Ramon Ramos III <email": null>
2015-08-22 23:15:08 -0400

    Add new route to create new card

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,10 +1,18 @@
 get "/deck/new" do
+  @user = User.find_by(id: session[:user_id])
+
   erb :"deck/create_deck"
 end
 
 post "/deck" do
   name = params[:deck_name]
-  @deck = Deck.create(name: name, creator_id: params[:user_id])
+  user_id = params[:user_id]
+  deck_img = params[:deck_img]
+  @deck = Deck.create(name: name, creator_id: user_id, deck_img: deck_img)
 
   redirect "/deck/#{@deck.id}"
+end
+
+get "/deck/:id/card/new" do
+
 end
