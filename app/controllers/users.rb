1ce98b18c67e51eb714ae63c4d242927496b67d2
Albalenys <albalenysd@gmail.com>
2015-08-23 23:55:25 -0400

    display user decks in profile

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,9 +10,10 @@ end
 
 get "/users/:id" do
   @user = User.find_by(id: params[:id])
+  @user_decks =  Deck.all.select { |deck| deck.creator_id == @user.id }
 	if params[:id].to_i == current_user.id
   		erb :"users/profile"
-  	else
+    else
   		erb :"auth/unauth"
   	end
 end
