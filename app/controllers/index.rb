8964648d041e0a5155059775324266c9998459e5
GLNRO <lg@glnro.com>
2015-08-24 01:36:19 -0400

    Remove whitespace and debugging statement

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,3 @@
-require 'pry'
 get '/' do
   decks = Deck.all
   erb :index, locals: {decks: decks}
@@ -24,7 +23,7 @@ post '/login' do
 end
 
 get '/logout' do
-  session.clear
+  logout!
   redirect "/"
 end
 
